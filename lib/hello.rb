ecc1f63f1c975160630cbf9f65c31259c42ac680
sarah hussain <sarahhussain17@yahoo.com>
2020-04-24 03:47:09 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,21 @@
-def hello_t []
-      i = 0
-      while i < array.length
-        yield(array[i])
-        i = i + 1
-      end
-      array
+def reverse_each_word_with_each(string)
+  # returns that same sentence with each word reversed in place.
+  #First solve it using .each Then write the same method using .collect to see the difference.
+  #turn the string into an array
+  #.reverse reverses an array
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
+  return_array.join(" ")
+end
 
-  hello_t(["Tim", "Tom", "Jim"]) do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
   end
+  test_array.join(" ")
+end
